e9ca40054ceecf03bef236ed2dc5126d0abe84a9
hjartland <tore@hjartland.com>
2025-02-12 16:22:41 +0100

    split images and links

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -41,3 +41,61 @@ def extract_markdown_links(text):
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
     return matches
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        
+        matches = extract_markdown_links(node.text)
+        if not matches:
+            new_nodes.append(node)
+            continue
+        
+        text = node.text
+        last_index = 0
+        for match in matches:
+            link_text, link_url = match
+            start_index = text.find(f'[{link_text}]({link_url})', last_index)
+            if start_index > last_index:
+                new_nodes.append(TextNode(text[last_index:start_index], TextType.TEXT))
+            
+            new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+            last_index = start_index + len(f'[{link_text}]({link_url})')
+        
+        if last_index < len(text):
+            new_nodes.append(TextNode(text[last_index:], TextType.TEXT))
+    
+    return new_nodes
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        
+        matches = extract_markdown_images(node.text)
+        if not matches:
+            new_nodes.append(node)
+            continue
+        
+        text = node.text
+        last_index = 0
+        for match in matches:
+            alt_text, image_url = match
+            start_index = text.find(f'![{alt_text}]({image_url})', last_index)
+            if start_index > last_index:
+                new_nodes.append(TextNode(text[last_index:start_index], TextType.TEXT))
+            
+            new_nodes.append(TextNode(alt_text, TextType.IMAGE, image_url))
+            last_index = start_index + len(f'![{alt_text}]({image_url})')
+        
+        if last_index < len(text):
+            new_nodes.append(TextNode(text[last_index:], TextType.TEXT))
+    
+    return new_nodes
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,10 @@
 import unittest
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from inline_markdown import (split_nodes_delimiter, 
+                             extract_markdown_images, 
+                             extract_markdown_links, 
+                             split_nodes_link, 
+                             split_nodes_image)
 
 class TestInlineMarkDown(unittest.TestCase):
     def test_code_block(self):
@@ -133,6 +137,71 @@ class TestInlineMarkDown(unittest.TestCase):
         text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
         expected = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
         self.assertListEqual(extract_markdown_links(text), expected)
+
+
+    def test_split_nodes_link_single(self):
+        node = TextNode("This is text with a link [to boot dev](https://www.boot.dev)", TextType.TEXT)
+        expected = [
+            TextNode("This is text with a link ", TextType.TEXT),
+            TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+        ]
+        self.assertEqual(split_nodes_link([node]), expected)
+
+    def test_split_nodes_link_multiple(self):
+        node = TextNode("Check [Google](https://google.com) and [GitHub](https://github.com)", TextType.TEXT)
+        expected = [
+            TextNode("Check ", TextType.TEXT),
+            TextNode("Google", TextType.LINK, "https://google.com"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("GitHub", TextType.LINK, "https://github.com"),
+        ]
+        self.assertEqual(split_nodes_link([node]), expected)
+
+    def test_split_nodes_image_single(self):
+        node = TextNode("Here is an image ![alt text](https://image.com/image.png)", TextType.TEXT)
+        expected = [
+            TextNode("Here is an image ", TextType.TEXT),
+            TextNode("alt text", TextType.IMAGE, "https://image.com/image.png"),
+        ]
+        self.assertEqual(split_nodes_image([node]), expected)
+
+    def test_split_nodes_image_multiple(self):
+        node = TextNode("Images: ![one](https://img1.com) and ![two](https://img2.com)", TextType.TEXT)
+        expected = [
+            TextNode("Images: ", TextType.TEXT),
+            TextNode("one", TextType.IMAGE, "https://img1.com"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("two", TextType.IMAGE, "https://img2.com"),
+        ]
+        self.assertEqual(split_nodes_image([node]), expected)
+
+    def test_split_nodes_link_no_links(self):
+        node = TextNode("This has no links.", TextType.TEXT)
+        expected = [node]
+        self.assertEqual(split_nodes_link([node]), expected)
+
+    def test_split_nodes_image_no_images(self):
+        node = TextNode("No images here.", TextType.TEXT)
+        expected = [node]
+        self.assertEqual(split_nodes_image([node]), expected)
+
+    def test_split_nodes_link_adjacent_links(self):
+        node = TextNode("[Google](https://google.com)[GitHub](https://github.com)", TextType.TEXT)
+        expected = [
+            TextNode("Google", TextType.LINK, "https://google.com"),
+            TextNode("GitHub", TextType.LINK, "https://github.com"),
+        ]
+        self.assertEqual(split_nodes_link([node]), expected)
+    
+    def test_split_nodes_image_adjacent_images(self):
+        node = TextNode("![one](https://img1.com)![two](https://img2.com)", TextType.TEXT)
+        expected = [
+            TextNode("one", TextType.IMAGE, "https://img1.com"),
+            TextNode("two", TextType.IMAGE, "https://img2.com"),
+        ]
+        self.assertEqual(split_nodes_image([node]), expected)
+    
+
 
 if __name__ == "__main__":
     unittest.main()
